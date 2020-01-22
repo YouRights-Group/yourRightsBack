a2e424e5de03f32a0ba42ea02bb20415fbd0ab73
Ana Román Rodríguez <ana.roman@ciberexperis.es>
2020-01-22 20:58:58 +0100

    Add security to some requests. Create method to reset Token.

--- src/main/java/com/yourrights/controllers/ProtestsController.java
+++ src/main/java/com/yourrights/controllers/ProtestsController.java
@@ -21,36 +21,37 @@ import io.swagger.annotations.ApiImplicitParam;
 @RestController
 @RequestMapping(Constants.PROTEST_REST)
 @CrossOrigin(origins = "*")
-// TODO Mirar crossOrigin
 public class ProtestsController {
 
-    @Autowired
-    private ProtestsService protestService;
+	@Autowired
+	private ProtestsService protestService;
 
-    @PostMapping(Constants.CREATE)
-    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
-    public void createProtest(@RequestBody Protest protest) {
-	protestService.createProtest(protest);
-    }
+	@PostMapping(Constants.CREATE)
+	@ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
+	public void createProtest(@RequestBody Protest protest) {
+		protestService.createProtest(protest);
+	}
 
-    @GetMapping(value = Constants.LIST + "/{page}")
-    public Protests getProtests(@PathVariable("page") int page) {
-	return protestService.getProtests(page - 1);
-    }
+	@GetMapping(value = Constants.LIST + "/{page}")
+	public Protests getProtests(@PathVariable("page") int page) {
+		return protestService.getProtests(page - 1);
+	}
 
-    @GetMapping(value = "{id}")
-    public Protest getProtest(@PathVariable("id") long id) {
-	return protestService.getProtest(id);
-    }
+	@GetMapping(value = "{id}")
+	@ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
+	public Protest getProtest(@PathVariable("id") long id) {
+		return protestService.getProtest(id);
+	}
 
-    @PostMapping(value = Constants.SEARCH)
-    public Protests searchProtest(@RequestBody SearchRequest request) {
-	return protestService.searchProtest(request);
-    }
+	@PostMapping(value = Constants.SEARCH)
+	public Protests searchProtest(@RequestBody SearchRequest request) {
+		return protestService.searchProtest(request);
+	}
 
-    @DeleteMapping(value = "delete/" + "/{id}")
-    public void deleteProtest(@PathVariable("id") long id) {
-	protestService.deleteProtest(id);
-    }
+	@DeleteMapping(value = Constants.DELETE + "/{id}")
+	@ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
+	public void deleteProtest(@PathVariable("id") long id) {
+		protestService.deleteProtest(id);
+	}
 
 }
